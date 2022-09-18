1ae377e0a5d0bd62451226ad70bf92cae3fd66e1
kombatov <kmbwork@yandex.ru>
2022-09-19 00:08:06 +0500

    Добавление шаблонов команд

---
+++ ClassMainSettings.py
@@ -0,0 +1,69 @@
+import os
+import json
+import sys
+
+class AnyErr(Exception):
+    pass
+
+
+# Класс для настроек, задаваемых пользователем через джесон
+class JsonUserSet:
+    def __init__(self, classmainset):  # Конструктор
+        self.ms = classmainset
+        self.__inifilename = os.path.join(self.ms.dircur, "hdmi44mtxset.json")
+        if not os.path.exists(self.__inifilename):
+            raise FileNotFoundError("Не найден файл: "+self.__inifilename)
+        with open(self.__inifilename, "r", encoding='utf-8') as f:
+            self.__inijson = json.load(f)
+
+    def getparams(self):
+        sparam = ""
+        try:
+            sparam = "dev_ipport"
+            self.ms.dev_ipport = self.__inijson.get(sparam).lower().strip()
+            self.ms.dev_ip, self.ms.dev_port = self.ms.dev_ipport.split(':')
+            self.ms.dev_port = int(self.ms.dev_port)
+            #Массив шаблонов команд
+            sparam = "command_templates"
+            self.ms.command_templates = self.__inijson.get(sparam)
+
+        except AnyErr as err:
+            print("Ошибка при считывании параметра " + sparam + "\n" + str(err))
+            raise
+
+    def setparams(self):
+        sparam = ""
+        try:
+            sparam = "dev_ipport"
+            self.__inijson[sparam] = self.ms.dev_ipport
+            sparam = "command_templates"
+            self.__inijson[sparam] = self.ms.command_templates
+            #Запись в файл
+            with open(self.__inifilename, 'w', encoding='utf-8') as f:
+                json.dump(self.__inijson, f, indent=2, ensure_ascii=False)
+        except AnyErr as err:
+            print("Ошибка при установке параметра " + sparam + "\n" + str(err))
+            raise
+
+# Класс для настроек приложения
+class MainSet:
+    def __init__(self):  # Конструктор
+        # Только винда и линух
+        if sys.platform.startswith('linux'):
+            self.oscur = "lin"
+        elif sys.platform.startswith('win'):
+            self.oscur = "win"
+        else:
+            print("Неподдерживаемая операционная система: " + sys.platform + "!")
+            raise
+        self.dev_ipport = ""
+        self.dev_ip = ""
+        self.dev_port = 0
+        self.command_templates = list()
+        self.dircur = os.path.dirname(os.path.realpath(__file__))
+        #установка начальных параметров
+        self.__jus = JsonUserSet(self)
+        self.__jus.getparams()
+
+    def __del__(self):
+        self.__jus.setparams()
--- hdmi_main.py
+++ hdmi_main.py
@@ -1,23 +1,21 @@
 #!/usr/bin/env python3
 
 import sys
-import io
 from PyQt5 import QtWidgets, QtCore, QtGui, uic
 import socket
-import hdmi_const
+import res.hdmi_const as hdmi_const
+import ClassMainSettings
 
-class hdmi_window(QtWidgets.QWidget):
+class main_window(QtWidgets.QWidget):
     def __init__(self, parent=None):
         self.__connected = False
-        self.__valout = ""
-        self.__valinp = ""
-        self.__ipport = ("192.168.0.221", 8899)
         QtWidgets.QWidget.__init__(self, parent)
 
         self.LoadMainUi()
 
         self.__labelRCdefault = self.labelRC.text()
         self.ActClick.triggered.connect(self.ActClickImpl)
+        self.ActSettings.triggered.connect(self.ActClickSettings)
         self.sock = socket.socket()
         self.sock.settimeout(2)
         self.TryConnected()
@@ -26,38 +24,62 @@ class hdmi_window(QtWidgets.QWidget):
         self.sock.close()
 
     def LoadMainUi(self):
-        QtCore.QDir.addSearchPath('dir_ui', '.')
-        self.uiFile = QtCore.QFile("dir_ui:hdmi_rc.ui")
+        #uic.loadUi('res/hdmi_rc.ui', self)
+        QtCore.QDir.addSearchPath('dir_ui', 'res')
+        self.uiFile = QtCore.QFile("dir_ui:main.ui")
         self.uiFile.open(QtCore.QFile.ReadOnly)
-        self.data = self.uiFile.readAll()
-        self.uiFile.close()
-        self.uiFile = io.BytesIO(bytes(self.data))
         uic.loadUi(self.uiFile, self)
 
     def TryConnected(self):
-        self.__pal = self.labelRC.palette()
+        __pal = self.labelRC.palette()
         try:
-            self.sock.connect(self.__ipport)
+            self.sock.connect((mainset.dev_ip, mainset.dev_port))
             self.__connected = True
             self.labelRC.setText(self.__labelRCdefault)
-            self.__pal.setColor(QtGui.QPalette.WindowText, QtGui.QColor("black"))
+            __pal.setColor(QtGui.QPalette.WindowText, QtGui.QColor("black"))
         except:
             self.__connected = False
-            self.labelRC.setText(self.__labelRCdefault + ' ' + str(self.__ipport))
-            self.__pal.setColor(QtGui.QPalette.WindowText, QtGui.QColor("red"))
-        self.labelRC.setPalette(self.__pal)
+            self.labelRC.setText(self.__labelRCdefault + ' ' + str(mainset.dev_ipport))
+            __pal.setColor(QtGui.QPalette.WindowText, QtGui.QColor("red"))
+        self.labelRC.setPalette(__pal)
 
     def ActClickImpl(self):
-        self.__valout, self.__valinp = self.ActClick.sender().objectName().lower()[6:].split("_")
+        self.PushButtons([self.ActClick.sender().objectName()])
+
+    def ActClickSettings(self):
+        contextMenu = QtWidgets.QMenu(self)
+        ActSettingsQuit = contextMenu.addAction("Выход")
+        ActSettingsQuit.triggered.connect(self.close)
+        ActSettingsSets = contextMenu.addAction("Настройки")
+        contextMenu.addSeparator()
+        for i in range(len(mainset.command_templates)):
+            newact = QtWidgets.QAction(mainset.command_templates[i]["caption"], contextMenu)
+            newact.setData(i)
+            newact.triggered.connect(self.ActTemplateApply)
+            contextMenu.addAction(newact)
+        contextMenu.popup(self.mapToGlobal(self.btnSettings.pos()))
+
+    def ActTemplateApply(self):
+        num_command = self.sender().data()
+        list_command =mainset.command_templates[num_command]["command"]
+        self.PushButtons(list_command)
+
+    def PushButtons(self, arrNames):
         if not self.__connected:
             self.TryConnected()
-        try:
-            self.sock.send(hdmi_const.outs[int(self.__valout)][self.__valinp])
-        except:
-            self.TryConnected()
+        for curname in arrNames:
+            __valout, __valinp = curname.lower()[6:].split("_")
+            try:
+                self.sock.send(hdmi_const.outs[int(__valout)][__valinp])
+            except:
+                self.TryConnected()
 
 
-app = QtWidgets.QApplication([])
-win = hdmi_window()
-win.show()
-sys.exit(app.exec())
+mainset = ClassMainSettings.MainSet()
+try:
+    app = QtWidgets.QApplication([])
+    win = main_window()
+    win.show()
+    sys.exit(app.exec())
+finally:
+    del mainset
